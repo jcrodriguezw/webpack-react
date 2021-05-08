aa9d46b16d81a2547171cb51b34ee3d0c4df26ec
Juan C. Rodriguez W <jcrodriguezw@gmail.com>
2021-05-08 07:27:02 -0500

    initial commit con webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/global.scss";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("app"));
